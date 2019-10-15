6ae80eebdf556a17da7e5fb7ac81590719df00b5
Carlos Eduardo <caduleonel1988@gmail.com>
2019-10-15 20:29:37 -0300

    Refatorados os metodos equals e hashcode da classe UserManga para não permitir a inclusão de mangas repetidos na conta do usuário (foi necessario implementar equals e hash code na classe Manga também). Falta verificar como mostrar uma mensagem para o usuario.

--- src/main/java/br/com/carlos/mangaorganizer/models/Manga.java
+++ src/main/java/br/com/carlos/mangaorganizer/models/Manga.java
@@ -85,4 +85,62 @@ public class Manga {
 	public void setSummary(String summary) {
 		this.summary = summary;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((chapters == null) ? 0 : chapters.hashCode());
+		result = prime * result + ((finalizationDate == null) ? 0 : finalizationDate.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((publicationDate == null) ? 0 : publicationDate.hashCode());
+		result = prime * result + ((status == null) ? 0 : status.hashCode());
+		result = prime * result + ((summary == null) ? 0 : summary.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Manga other = (Manga) obj;
+		if (chapters == null) {
+			if (other.chapters != null)
+				return false;
+		} else if (!chapters.equals(other.chapters))
+			return false;
+		if (finalizationDate == null) {
+			if (other.finalizationDate != null)
+				return false;
+		} else if (!finalizationDate.equals(other.finalizationDate))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (publicationDate == null) {
+			if (other.publicationDate != null)
+				return false;
+		} else if (!publicationDate.equals(other.publicationDate))
+			return false;
+		if (status != other.status)
+			return false;
+		if (summary == null) {
+			if (other.summary != null)
+				return false;
+		} else if (!summary.equals(other.summary))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		return true;
+	}
 }
--- src/main/java/br/com/carlos/mangaorganizer/models/UserManga.java
+++ src/main/java/br/com/carlos/mangaorganizer/models/UserManga.java
@@ -66,7 +66,6 @@ public class UserManga implements Serializable{
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((manga == null) ? 0 : manga.hashCode());
 		return result;
 	}
@@ -80,11 +79,6 @@ public class UserManga implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		UserManga other = (UserManga) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
 		if (manga == null) {
 			if (other.manga != null)
 				return false;
@@ -92,5 +86,7 @@ public class UserManga implements Serializable{
 			return false;
 		return true;
 	}
+
+
 
 }
