669f157e0fc5992e2aff933c20f52f4c7fd23559
Carlos Eduardo <caduleonel1988@gmail.com>
2019-10-23 20:42:19 -0300

    Aumentando a quantidade de caracteres do summario do manga.

--- src/main/java/br/com/carlos/mangaorganizer/models/Manga.java
+++ src/main/java/br/com/carlos/mangaorganizer/models/Manga.java
@@ -4,6 +4,7 @@ import java.util.Calendar;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -19,6 +20,8 @@ public class Manga {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String title;
+	
+	@Column(length = 2000)
 	private String summary;
 	
 	@OneToMany(cascade = {CascadeType.ALL, CascadeType.REMOVE}, mappedBy = "manga")
